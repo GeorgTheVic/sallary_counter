9d68150d8846e9b94436be1c3f9b029025ef15ff
Samsonov-G <Samsonov-G>
2025-09-15 17:50:41 +0500

    acrueted tenge to dollar price

--- assets/script.js
+++ assets/script.js
@@ -10,42 +10,38 @@ document.addEventListener('DOMContentLoaded', function() {
   ltw_value = 0,
   lpw_value = 0,
   usd_rate = 5.15,
-  tng_to_usd_rate = 550,
+  tng_to_usd_rate = 540,
   rub_rate = 360
 
   conversion.addEventListener('change', function() {
     if (conversion.checked) {
     conversion_value = 0.71
-    console.log('ok')
   } else {
     conversion_value = 0
-    console.log('zero')
   }
   })
 
   ltw.addEventListener('change', function() {
     if (ltw.checked) {
     ltw_value = 0.71
-    console.log('ok')
   } else {
     ltw_value = 0
-    console.log('zero')
   }
   })
 
   lpw.addEventListener('change', function() {
     if (lpw.checked) {
     lpw_value = 0.71
-    console.log('ok')
   } else {
     lpw_value = 0
-    console.log('zero')
   }
   })
 
   amount_lessons.addEventListener('input', function() {
-    usd.innerHTML = amount_lessons.value * (usd_rate + conversion_value + ltw_value + lpw_value)
-    tng.innerHTML = usd.innerHTML * tng_to_usd_rate
-    rub.innerHTML = amount_lessons.value * (rub_rate + conversion_value + ltw_value + lpw_value)
+    usd.innerHTML = (amount_lessons.value * (usd_rate + conversion_value + ltw_value + lpw_value)).toFixed(2)
+
+    tng.innerHTML = (usd.innerHTML * tng_to_usd_rate).toFixed(2)
+
+    rub.innerHTML = (amount_lessons.value * (rub_rate + conversion_value + ltw_value + lpw_value)).toFixed(2)
   })
 });
