7752894f254faf15bebdd94caab8a79a3c1fe252
Samsonov-G <Samsonov-G>
2025-09-16 17:07:20 +0500

    changed js

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const amount_lessons = document.querySelector('#amount_lessons'),
-  usd = document.querySelector('#usd'),
-  tng = document.querySelector('#tng'),
-  rub = document.querySelector('#rub'),
-  conversion = document.querySelector('#conversion'),
-  ltw =  document.querySelector('#ltw'),
-  lpw = document.querySelector('#lpw')
+document.addEventListener('DOMContentLoaded', function () {
+
   let conversion_value = 0,
-  ltw_value = 0,
-  lpw_value = 0,
-  rub_conversion_value = 0,
-  rub_ltw_value = 0,
-  rub_lpw_value = 0,
-  usd_rate = 5.15,
-  tng_to_usd_rate = 540,
-  rub_rate = 360
+    ltw_value = 0,
+    lpw_value = 0,
+    rub_conversion_value = 0,
+    rub_ltw_value = 0,
+    rub_lpw_value = 0,
+    usd_rate = 5.15,
+    tng_to_usd_rate = 540,
+    rub_rate = 360
 
-  conversion.addEventListener('change', function() {
+  const amount_lessons = document.querySelector('#amount_lessons'),
+    usd = document.querySelector('#usd'),
+    tng = document.querySelector('#tng'),
+    rub = document.querySelector('#rub'),
+    conversion = document.querySelector('#conversion'),
+    ltw = document.querySelector('#ltw'),
+    lpw = document.querySelector('#lpw')
+
+  conversion.addEventListener('change', function () {
     if (conversion.checked) {
-    conversion_value = 0.71
-    rub_conversion_value = 50
-  } else {
-    conversion_value = 0
-    rub_conversion_value = 0
-  }
+      conversion_value = 0.71
+      rub_conversion_value = 50
+    } else {
+      conversion_value = 0
+      rub_conversion_value = 0
+    }
   })
 
-  ltw.addEventListener('change', function() {
+  ltw.addEventListener('change', function () {
     if (ltw.checked) {
-    ltw_value = 0.71
-    rub_ltw_value = 50
-  } else {
-    ltw_value = 0
-    rub_ltw_value = 0
-  }
+      ltw_value = 0.71
+      rub_ltw_value = 50
+    } else {
+      ltw_value = 0
+      rub_ltw_value = 0
+    }
   })
 
-  lpw.addEventListener('change', function() {
+  lpw.addEventListener('change', function () {
     if (lpw.checked) {
-    lpw_value = 0.71
-    rub_lpw_value = 50
-  } else {
-    lpw_value = 0
-    rub_lpw_value = 0
-  }
+      lpw_value = 0.71
+      rub_lpw_value = 50
+    } else {
+      lpw_value = 0
+      rub_lpw_value = 0
+    }
   })
 
-  amount_lessons.addEventListener('input', function() {
+  amount_lessons.addEventListener('input', function () {
     usd.innerHTML = (amount_lessons.value * (usd_rate + conversion_value + ltw_value + lpw_value)).toFixed(2)
 
     tng.innerHTML = (usd.innerHTML * tng_to_usd_rate).toFixed(2)
